8272121fa136efcb3b4f9430ff91b45d355bf579
salvatore <146131094+salvatore47@users.noreply.github.com>
2023-11-04 18:31:18 +0000

    Add files via upload

---
+++ eserci5.js
@@ -0,0 +1,17 @@
+// Esercizio Numero  1  non  eseguito  per  mancanza  di  conoscenza  della funzione  e di math non  approfondita  in  maniera  cosi elevata  in  lezione . 
+let trhow = prompt('how many trhow you whant')
+let trhow2 = prompt('how many trhow you whant')
+function gioco (Player1,Player2,winner){
+  winner = Player1 = trhow * 6 , Player2 = trhow2 * 6;
+  if  (Player1 > Player2 )
+   console.log(Player1)
+ else (Player1 < Player2)
+ console.log(Player2)
+s
+
+}
+console.log(gioco(1))
+
+//esercizio numero 2  non  eseguito  mancanza  di  conoscenza  della  funzione    e  di  non  essere  approfondita  in  maniera  elavata  in  lezione 
+  
+// esercizio  numero  3  idem   mancanza  di  competenze  per eseguire  la  lezione .
